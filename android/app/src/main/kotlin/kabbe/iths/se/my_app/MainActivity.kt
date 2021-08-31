d6f5f8974bb6c92a72473f6a5e35b39966edbde8
Kabbej <kabbe.jartun@gmail.com>
2021-08-31 10:10:36 +0200

    uploading first test with buttons and icons

---
+++ android/app/src/main/kotlin/kabbe/iths/se/my_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package kabbe.iths.se.my_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home()
+  ));
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold (
+      appBar: AppBar (
+        title: Text ('My first app'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center (
+        child:IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.alternate_email),
+        )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {  },
+        child: Text('click'),
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}
+
+
